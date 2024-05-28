381e4e3af6279119810114323a5a57e93117396d
leejuyeon <zoo0406.jyl@gmail.com>
2024-05-28 16:50:42 +0900

    유저테이블 참조하는 부분 삭제

--- models/consumptionHistoryDB.js
+++ models/consumptionHistoryDB.js
@@ -1,9 +1,8 @@
 const { db } = require("../db");
 
 exports.getConsumptionHistory = async (cHistoryID) => {
-    let sql = `SELECT ch.cHistoryID cHistoryID, ch.userID userID, ch.date date , ch.amount amount, ch.content content, ch.category category, u.nickname nickname 
-    FROM consumptionHistory ch, user u 
-    WHERE cHistoryID=${cHistoryID} && ch.userID = u.userID`;
+    let sql = `SELECT ch.cHistoryID cHistoryID, ch.userID userID, ch.date date , ch.amount amount, ch.content content, ch.category category, 
+    FROM consumptionHistory ch WHERE cHistoryID=${cHistoryID}`;
     let [rows, fields] = await db.query(sql);
     console.log(rows);
     return rows;
